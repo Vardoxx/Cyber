31797d08929ba58956f8e55c3f54a4f2c4b3ae2a
Vardoxx <maksgavrilov956@gmail.com>
2024-08-07 18:29:50 +0300

    smaller banners is done

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Heart from "./img/Header/Heart.png";
 import ShoppingCart from "./img/Header/ShoppingCart.png";
 import User from "./img/Header/User.png";
 import IphoneBanner from "./img/IphoneBanner/IphoneBanner.png";
+import PlayStation from "./img/SmallBanners/PlayStation.png";
+import Headphones from "./img/SmallBanners/Headphones.png";
+import VisionPro from "./img/SmallBanners/VisionPro.png";
+import MacBookPro from "./img/SmallBanners/MacBookPro.png";
 
 const App = () => {
   return (
@@ -70,10 +74,69 @@ const App = () => {
               <p className="iphone-banner__paragraph">
                 Created to change everything for the better. For everyone
               </p>
-              <a className="iphone-banner__button">Shop Now</a>
+              <a className="iphone-banner__btn">Shop Now</a>
             </div>
             <div className="iphone-banner__img">
-              <img src={IphoneBanner} alt="" />
+              <img src={IphoneBanner} alt="IphoneBanner" />
+            </div>
+          </div>
+        </div>
+        <div className="main__smaller-banners _smaller-banners__container">
+          <div className="smaller-banners">
+            <div className="smaller-banner__left-side">
+              <div className="left-side__top-banner">
+                <div className="top-banner__img">
+                  <img src={PlayStation} alt="PlayStation}" />
+                </div>
+                <div className="top-banner__text">
+                  <h1 className="top-banner__title">Playstation 5</h1>
+                  <p className="top-banner__paragrap">
+                    Incredibly powerful CPUs, GPUs, and an SSD with integrated
+                    I/O will redefine your PlayStation experience.
+                  </p>
+                </div>
+              </div>
+              <div className="left-side__bottom-banners">
+                <div className="bottom-banners__left-banner">
+                  <div className="left-banner__img">
+                    <img src={Headphones} alt="Headphones" />
+                  </div>
+                  <div className="left-banner__text">
+                    <h1 className="left-banner__title">Apple AirPods Max</h1>
+                    <p className="left-banner__paragraph">
+                      Computational audio. Listen, it's powerful
+                    </p>
+                  </div>
+                </div>
+                <div className="bottom-banners__right-banner">
+                  <div className="right-banner__img">
+                    <img src={VisionPro} alt="" />
+                  </div>
+                  <div className="right-banner__text">
+                    <h1 className="right-banner__title">Apple Vision Pro</h1>
+                    <p className="right-banner__paragraph">
+                      An immersive way to experience entertainment
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="smaller-banner__right-side">
+              <div className="right-side__big-banner">
+                <div className="big-banner__text">
+                  <h1 className="big-banner__title">
+                    Macbook <span>Air</span>
+                  </h1>
+                  <p className="big-banner__paragraph">
+                    The new 15-inch MacBook Air makes room for more of what you
+                    love with a spacious Liquid Retina display.
+                  </p>
+                  <button className="big-banner__btn">Shop Now</button>
+                </div>
+                <div className="big-banner__img">
+                  <img src={MacBookPro} alt="MacBookPro" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
